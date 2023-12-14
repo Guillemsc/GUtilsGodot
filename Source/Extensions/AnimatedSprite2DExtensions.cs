d07e376a872221a048ea6d3a63b79226055484b7
Guillem Sunyer <gsunyercaldu@gmail.com>
2023-12-14 21:49:57 +0100

    Extensions

--- Source/Extensions/AnimatedSprite2DExtensions.cs
+++ Source/Extensions/AnimatedSprite2DExtensions.cs
@@ -1,10 +1,25 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Godot;
 
 namespace GUtilsGodot.Extensions;
 
 public static class AnimatedSprite2DExtensions
 {
+    public static async Task AwaitCompletition(this AnimatedSprite2D animatedSprite2D, CancellationToken cancellationToken)
+    {
+        while (animatedSprite2D.IsPlaying())
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            await TaskExtensions.GodotYield();
+        }
+    }
+    
     public static void Play<T>(this AnimatedSprite2D animatedSprite2D, T animationName) where T : Enum
     {
         animatedSprite2D.Play(animationName.ToString());
