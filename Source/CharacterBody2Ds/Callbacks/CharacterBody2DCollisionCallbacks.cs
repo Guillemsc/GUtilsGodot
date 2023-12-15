d669d1a802219925c0b09293ffa68746fe4d15f2
Guillem Sunyer <gsunyercaldu@gmail.com>
2023-12-16 00:20:35 +0100

    Collision callbacks

---
+++ Source/CharacterBody2Ds/Callbacks/CharacterBody2DCollisionCallbacks.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+using GUtils.Extensions;
+
+namespace GUtilsGodot.CharacterBody2Ds.Callbacks;
+
+public partial class CharacterBody2DCollisionCallbacks : Node2D
+{
+    [Export] public CharacterBody2D? CharacterBody2D;
+    
+    readonly HashSet<Node2D> CollidingNodes = new();
+    readonly HashSet<Node2D> CheckingCollidingNodes = new();
+    readonly HashSet<Node2D> AlreadyCheckedCollidingNodes = new();
+
+    public event Action<Node2D, KinematicCollision2D>? OnEnter;
+    public event Action<Node2D, KinematicCollision2D>? OnStay;
+    public event Action<Node2D>? OnExit;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (CharacterBody2D == null)
+        {
+            return;
+        }
+        
+        AlreadyCheckedCollidingNodes.Clear();
+        CheckingCollidingNodes.Clear();
+        CheckingCollidingNodes.AddRange(CollidingNodes);
+        
+        for (int i = 0; i < CharacterBody2D.GetSlideCollisionCount(); i++)
+        {
+            KinematicCollision2D kinematicCollision2D = CharacterBody2D.GetSlideCollision(i);
+            GodotObject collider = kinematicCollision2D.GetCollider();
+
+            if (collider is not Node2D node2D)
+            {
+                return;
+            }
+
+            bool alreadyChecked = AlreadyCheckedCollidingNodes.Contains(node2D);
+
+            if (alreadyChecked)
+            {
+                continue;
+            }
+
+            AlreadyCheckedCollidingNodes.Add(node2D);
+                
+            bool existed = CheckingCollidingNodes.Remove(node2D);
+
+            if (!existed)
+            {
+                CollidingNodes.Add(node2D);
+                OnEnter?.Invoke(node2D, kinematicCollision2D);
+            }
+            else
+            {
+                OnStay?.Invoke(node2D, kinematicCollision2D);
+            }
+        }
+
+        foreach (Node2D checking in CheckingCollidingNodes)
+        {
+            CollidingNodes.Remove(checking);
+            OnExit?.Invoke(checking);
+        }
+    }
+}
