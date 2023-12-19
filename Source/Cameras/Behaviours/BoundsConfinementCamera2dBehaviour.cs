2c36ce012f82ee0dd5b06c0367d43d7ff0b039cc
Guillem Sunyer <gsunyercaldu@gmail.com>
2023-12-19 20:09:58 +0100

    Fixed issue

--- Source/Cameras/Behaviours/BoundsConfinementCamera2dBehaviour.cs
+++ Source/Cameras/Behaviours/BoundsConfinementCamera2dBehaviour.cs
@@ -1,17 +1,16 @@
 using Godot;
 using GUtils.Optionals;
 using GUtilsGodot.Extensions;
-using GUtilsGodot.Rects.Nodes;
 
 namespace GUtilsGodot.Cameras.Behaviours;
 
 public sealed class BoundsConfinementCamera2dBehaviour : ICamera2dBehaviour
 {
-    Optional<Rect2dNode> _boundsRect;
+    Optional<Rect2> _boundsRect;
     
     public void Tick(float dt, bool previousStateValid, Camera2D camera2D)
     {
-        bool hasBoundsRect = _boundsRect.TryGet(out Rect2dNode rect2dNode);
+        bool hasBoundsRect = _boundsRect.TryGet(out Rect2 rect);
 
         if (!hasBoundsRect)
         {
@@ -19,7 +18,6 @@ public sealed class BoundsConfinementCamera2dBehaviour : ICamera2dBehaviour
         }
 
         Vector2 position = camera2D.GlobalPosition;
-        Rect2 rect = rect2dNode.GetGlobalRect();
         Vector2 viewportSize = camera2D.GetGameViewportRect().Size;
 
         bool insideBounds = Camera2DExtensions.IsCameraInsideBounds(position, viewportSize, rect);
@@ -32,7 +30,7 @@ public sealed class BoundsConfinementCamera2dBehaviour : ICamera2dBehaviour
         camera2D.GlobalPosition = Camera2DExtensions.GetCameraPositionInsideBounds(position, viewportSize, rect);
     }
     
-    public void SetBounds(Rect2dNode bounds)
+    public void SetBounds(Rect2 bounds)
     {
         _boundsRect = bounds;
     }
